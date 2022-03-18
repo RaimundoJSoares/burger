d3c7413901e159d61cc8cda8cc2be2112ddd2ea3
RaimundoJSoares <raianimes1000@gmail.com>
2022-03-17 23:41:24 -0400

    estilizando

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <Navbar />
+  <Navbar logo='logo_src' :alt="app_name"/>
   <router-view/>
   <Footer />
  </div> 
@@ -14,9 +14,31 @@ export default {
   components :  {
     Navbar,
     Footer
+  },
+  data() {
+    return {
+      logo_src: "img/logo.png",
+      app_name : "Make your Burger"
+    }
   }
 }
 </script>
 <style>
+* {
+  font-family: Helvetica, sans-serif;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
 
 </style>
